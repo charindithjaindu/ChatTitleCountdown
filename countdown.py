82b6279c46d66cffff784cc45e5f19bfa35ae6a4
charindithjaindu <75799963+charindithjaindu@users.noreply.github.com>
2021-05-13 10:20:24 +0530

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -8,35 +8,21 @@ cnl=os.getenv("GROUP_ID")
 session=os.getenv("SESSION")
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
-date_of_end=os.getenv("DATE")
 
-paras=date_of_end.split(' ')
-
-def diff_month(d1, d2):
-    return (d1.year - d2.year) * 12 + d1.month - d2.month
 
 def countdown(stop):
     while True:
         difference = stop - datetime.datetime.now()
-        count_hours, rem = divmod(difference.seconds, 3600)
-        count_minutes, count_seconds = divmod(rem, 60)
-        if difference.days == 0 and count_hours == 0 and count_minutes == 0 and count_seconds == 0:
-            print("Good bye!")
-            break
-        count_months=diff_month(stop,datetime.datetime.now())
         count_weeks=int(difference.days/7)
-        final_str=(str(count_months) + " months | "
-        	  + str(count_weeks) + " weeks | "
+        final_str=(str(int(difference.days/30)) + " months | "
+              + str(count_weeks) + " weeks | "
               + str(difference.days) + " days | "
-              +str(difference.days*24)+" hours | "
-              +str(difference.days*24*60)+" mins"
+              +str(int(difference.days*24+difference.seconds/3600))+" hours | "
+              +str(int(difference.days*24*60+difference.seconds/60))+" mins"
               )
         return final_str
 
-for i in range(len(paras)):
-	paras[i]=int(paras[i])
-	
-exam_date = datetime.datetime(int(paras[0]), int(paras[1]), int(paras[2]), int(paras[3]), int(paras[4]), int(paras[5]))
+exam_date = datetime.datetime(2021, 10 ,4 ,7, 0 ,0)
 
 print(countdown(exam_date))
 
@@ -44,4 +30,4 @@ with Client(session, api_id, api_hash) as app:
 	while True:
 	    app.set_chat_title(cnl,countdown(exam_date))
 	    print('changes')
-	    time.sleep(60)
+	    time.sleep(86400)
